1dcff6b4be6c8c8fd4af7531605dbb47c5a74e4f
abdurraihan <abdurraihan898@gmail.com>
2023-06-07 16:45:50 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // Testing
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: any, res: any) => {
   res.send('working university management  app ')
 })
 
