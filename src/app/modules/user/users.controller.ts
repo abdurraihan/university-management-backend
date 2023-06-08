c17f5dbb3cb3ceed6912fe84215ce76fd5dcd531
abdurraihan <abdurraihan898@gmail.com>
2023-06-08 18:24:38 +0600

    after costomize logger

--- src/app/modules/user/users.controller.ts
+++ src/app/modules/user/users.controller.ts
@@ -1,10 +1,10 @@
-import { Response, Request } from 'express'
+import { Request, Response } from 'express'
 import usersService from './users.service'
 
 const createUser = async (req: Request, res: Response) => {
   try {
     const { user } = req.body
-    const result = await usersService.createUser(user)
+    const result = await usersService.createUserService(user)
 
     res.status(200).json({
       success: true,
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
-import config from './config/index'
 import app from './app'
+import config from './config/index'
+import { errorLogger, logger } from './shared/logger'
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log(`database connected successfully `)
+    logger.info(`database connected successfully `)
 
     app.listen(config.port, () => {
-      console.log(`Application  listening on port ${config.port}`)
+      logger.info(`Application  listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log(`failed to connect database ${config.port}`)
+    errorLogger.error(`failed to connect database ${config.port}`)
   }
 }
 
